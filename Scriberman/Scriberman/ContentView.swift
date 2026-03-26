e0692fdd0d7a88411eb22834f2c9df2d07a5e579
stas <@ohshie>
2026-03-26 20:51:45 +0100

    Initialize Xcode project for Scriberman with SwiftUI and package dependencies

---
+++ Scriberman/Scriberman/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Scriberman")
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scriberman/Scriberman/ScribermanApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import FluidAudio
+
+@main
+struct ScribermanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
